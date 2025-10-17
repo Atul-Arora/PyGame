d004494fe3eff17a1f9eff8c41acd5c93b2e04c8
Atul <atularora0207@gmail.com>
2025-10-18 04:05:05 +0530

    Positioning with rectangle

---
+++ pygame7.py
@@ -0,0 +1,52 @@
+#Rectangles in python are really import and can actually quite a few things - the basic two are 
+#Precise positioning of surface 
+#Basic Collisions 
+#Surface for image information - Rectangle for placement 
+#https://ibb.co/Kpd4DRTp
+import pygame 
+from sys import exit
+
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))  
+clock = pygame.time.Clock()  
+test_font=pygame.font.Font("font/Pixeltype.ttf",50)
+
+pygame.display.set_caption("Runner")
+
+sky_surface = pygame.image.load("Graphics/sky.png").convert() 
+ground_surface = pygame.image.load("Graphics/ground.png").convert()
+text_surface=test_font.render("My Game",False,"Black").convert()
+
+
+snail_surf=pygame.image.load("Graphics/snail/snail1.png",).convert_alpha()
+snail_x_pos = 600 
+snail_rect = snail_surf.get_rect(bottomright = (600,300) )
+
+player_surf=pygame.image.load("Graphics/Player/player_walk_1.png").convert_alpha()
+#player_rect = player_rect(left,top,width,height) 
+#we can create a rectangle this way but we dont generaly see something like this cuz we need a rectangle of exact size ofsurface
+player_rect = player_surf.get_rect(midbottom = (80,300)) #This takes surface and draws rectangle around it 
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() 
+            exit() 
+    
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+
+    snail_rect.x -= 4
+    if snail_rect.right<=0:
+        snail_rect.left=800
+    screen.blit(snail_surf,(snail_rect))
+ 
+    
+    screen.blit(player_surf,player_rect) 
+    # Using rectangle to place surface
+    
+
+    pygame.display.update()
+    clock.tick(60)  
