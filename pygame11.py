6f67b51fa5854f854b5249aa069ef1824b71fe44
Atul <atularora0207@gmail.com>
2025-10-25 20:34:19 +0530

    minor changes and colour code with rgb and hex

--- pygame11.py
+++ pygame11.py
@@ -1,10 +1,4 @@
-#Using rectangle to centre the score text 
-#Drawing with pygame.draw
-#Also renamed text_surface to score_surface
-#In pygame there is a sub module called draw and can pass different attributes like circle , lines , rectangle , pointes , ellipse etc
-#Also checkout pygame doumentation on pygame.draw  
-
-"""EXCERCISE - Go through the documentation and draw a straight line from the top left to the bottom right of the screen"""
+#Colours with the use of rgb and hex codes
 
 import pygame
 from sys import exit
@@ -20,7 +14,7 @@ pygame.display.set_caption("Runner")
 sky_surface = pygame.image.load("Graphics/sky.png").convert() 
 ground_surface = pygame.image.load("Graphics/ground.png").convert()
 
-score_surf = score_font.render("My Game",False,"Black").convert()
+score_surf = score_font.render("My Game",False,(64,64,64)).convert()
 score_rect = score_surf.get_rect(center = (400,50))
 
 
@@ -31,7 +25,6 @@ snail_rect = snail_surf.get_rect(bottomright = (600,300) )
 player_surf = pygame.image.load("Graphics/Player/player_walk_1.png").convert_alpha()
 
 player_rect = player_surf.get_rect(midbottom = (80,300)) 
-width = 825 
 
 while True:
     for event in pygame.event.get():
@@ -43,12 +36,10 @@ while True:
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    #pygame.draw tells pygame to draw and after that we defined what shape we want to draw 
-    pygame.draw.rect(screen,"Pink",score_rect) 
-    #Draws a filled rectangle (solid pink).
-    pygame.draw.rect(screen,"Pink",score_rect,10)  
-    #Draws only the border (outline) of the same rectangle, with a line thickness of 10 pixels. 
-    
+    pygame.draw.rect(screen,"#c0e8ec",score_rect) 
+    pygame.draw.rect(screen,"#c0e8ec",score_rect,10)  
+     
+
     screen.blit(score_surf,score_rect)
 
     snail_rect.x -= 4
@@ -57,9 +48,7 @@ while True:
     screen.blit(snail_surf,(snail_rect))
     screen.blit(player_surf,player_rect) 
     
-    width -= 5
-    pygame.draw.line(screen,"Gold",(0,0),(800,400),width)  
-    # you can also give one vector as pygame.mouse.get.pos() and you will get a line that always follows a mouse 
+    
 
     pygame.display.update()
     clock.tick(60)  
