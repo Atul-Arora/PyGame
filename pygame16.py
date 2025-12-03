fa407eaa0f16b622252bf0b3b179907c68bd6114
Atul <atularora0207@gmail.com>
2025-12-03 20:11:10 +0530

    collsion fix , fly enemy

---
+++ pygame16.py
@@ -0,0 +1,153 @@
+# Current problems in our Code
+# 1. Collsion dont work anymore 
+# 2. We dont delete rectangles when they leave the screen
+# 3. Only one type of enemy 
+
+# For fixing the collision logic
+# cycle through the obstacles rects 
+# check if player collison 
+
+
+import pygame
+from sys import exit
+from random import randint
+
+def dispay_score():
+
+    current_time = int(pygame.time.get_ticks()/1000) - start_time
+    score_surf = test_font.render(f'Score: {current_time}',False,(64,64,64))
+    score_rect = score_surf.get_rect(center =(400,50))
+    screen.blit(score_surf,score_rect)
+    return current_time
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            if obstacle_rect.bottom == 300:
+                screen.blit(snail_surf,obstacle_rect)
+
+            else: screen.blit(fly_surf,obstacle_rect)
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+        return obstacle_list
+
+    else: return []
+
+def collsions(player,obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect): 
+                return False
+            
+    return True
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))  
+clock = pygame.time.Clock()  
+test_font = pygame.font.Font("font/Pixeltype.ttf",50)
+game_active = False
+start_time = 0
+score = 0
+
+
+pygame.display.set_caption("Runner")
+
+sky_surface = pygame.image.load("Graphics/sky.png").convert() 
+ground_surface = pygame.image.load("Graphics/ground.png").convert()
+
+# Obstacles
+snail_surf = pygame.image.load("Graphics/snail/snail1.png",).convert_alpha()
+
+fly_surf = pygame.image.load("Graphics/Fly/Fly1.png").convert_alpha()
+obstacle_rect_list=[]
+
+
+player_surf = pygame.image.load("Graphics/Player/player_walk_1.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
+player_gravity = 0
+
+# Intro Screen 
+player_stand = pygame.image.load("Graphics/Player/player_stand.png").convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand,0,2)
+player_stand_rect = player_stand.get_rect(center = (400,200));
+
+game_name = test_font.render("Pixel Runner","False",(111,196,169))
+game_name_rect = game_name.get_rect(center=(400,80))
+
+game_msg = test_font.render("Press space to run", False ,(111,196,169))
+game_msg_rect = game_msg.get_rect(center=(400,320))
+
+#Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer,1500)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() 
+            exit() 
+
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                    if player_rect.collidepoint(event.pos) and player_rect.bottom == 300 : 
+                        player_gravity = -20
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom == 300 :
+                    player_gravity = -21
+
+            if event.type == obstacle_timer:
+                if randint(0,1):  #Would give us 0 or 1 (0=False 1=True)
+                    obstacle_rect_list.append(snail_surf.get_rect(bottomright = (randint(900,1100),300)))
+                else:
+                    obstacle_rect_list.append(fly_surf.get_rect(bottomright = (randint(900,1100),207)))
+        
+        else: 
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE: 
+                game_active =1 
+                
+                start_time=int(pygame.time.get_ticks()/1000)
+
+       
+
+               
+    if game_active:
+        screen.blit(sky_surface,(0,0))
+        screen.blit(ground_surface,(0,300))
+        
+        score = dispay_score()
+
+       
+
+        # Player
+        player_gravity += 0.88
+        player_rect.y += player_gravity
+        if player_rect.bottom >=300 : player_rect.bottom = 300
+        screen.blit(player_surf,player_rect) 
+
+        #Obstacle movement 
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
+        # Collision
+        game_active = collsions(player_rect,obstacle_rect_list)
+    else:
+        screen.fill((94,129,162))
+        screen.blit(player_stand,player_stand_rect)
+        obstacle_rect_list.clear() #So player doesnt get stuck on collsion when restarting
+        player_rect.midbottom = (80,300)
+        player_gravity=0
+
+
+        score_message = test_font.render(f"Your Score: {score}", False ,(111,196,169))
+        score_message_rect = score_message.get_rect(center=(400,330))
+        screen.blit(game_name,game_name_rect)
+        if score == 0: screen.blit(game_msg,game_msg_rect)
+        else: screen.blit(score_message,score_message_rect)
+       
+
+
+    pygame.display.update()
+    clock.tick(60)  
+
+
